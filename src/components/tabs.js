7419ed44fd4f2d24898139536dbed99105fe0ec3
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-07-18 17:35:08 -0400

    Tabs component created and appended with tabsAppender

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,6 +15,17 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+  const topicsDiv = document.createElement('div');
+  topicsDiv.classList.add('topics');
+
+  topics.forEach(topic => {
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = topic;
+    topicsDiv.appendChild(tab);
+  });
+
+  return topicsDiv;
 }
 
 const tabsAppender = (selector) => {
@@ -23,6 +36,14 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  const cssSelector = document.querySelector(selector);
+  axios.get('http://localhost:5000/api/topics')
+    .then(response => {
+      const topicsArr = response.data.topics;
+      const topicTabs = Tabs(topicsArr);
+      cssSelector.appendChild(topicTabs);
+    })
+    .catch(error => console.log(error));
 }
 
 export { Tabs, tabsAppender }
